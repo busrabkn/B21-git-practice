e87d14ecb8d464bcef8f33270efb52cca1bf4a4e
busrabkn <74192539+busrabkn@users.noreply.github.com>
2021-01-26 14:18:19 -0500

    added some tasks

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("A");
+    }
+}
